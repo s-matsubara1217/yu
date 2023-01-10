fec3866dde79169d0eef53325f7859295bbe5b2c
s-matsubara <mtytsy15301217@gmail.com>
2023-01-10 11:25:32 +0900

    a

---
+++ js/company.js
@@ -0,0 +1,12 @@
+/*----------------------------------------
+	セパレートイメージ
+----------------------------------------*/
+$(function () {
+	$('.separateImgs__list').infiniteslide({
+		direction: 'left',
+		// speed: 40,
+		speed: 0,
+		clone: 3,
+		pauseonhover: false
+	});
+});
